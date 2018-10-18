9529da49f135577fed2e0822ca57a231a5f2757e
Cyril Alispach <cyril.alispach@unige.ch>
2018-10-18 09:28:19 +0200

    pep8

--- histogram/tests/test_histogram1d.py
+++ histogram/tests/test_histogram1d.py
@@ -40,12 +40,15 @@ def test_add():
         histo_1.fill(data)
         histo_2.fill(data)
 
+    overflow_total = histo_1.overflow.sum() + histo_2.overflow.sum()
+    underflow_total = histo_1.underflow.sum() + histo_2.underflow.sum()
+
     new_hist = histo_1 + histo_2
     assert new_hist.data.sum() == (histo_1.data.sum() + histo_2.data.sum())
     assert (new_hist.bins == histo_1.bins).all()
     assert (new_hist.bins == histo_2.bins).all()
-    assert new_hist.overflow.sum() == histo_1.overflow.sum() + histo_2.overflow.sum()
-    assert new_hist.underflow.sum() == histo_1.underflow.sum() + histo_2.underflow.sum()
+    assert new_hist.overflow.sum() == overflow_total
+    assert new_hist.underflow.sum() == underflow_total
 
 
 def test_save_and_load():
