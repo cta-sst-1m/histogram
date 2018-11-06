1b5cf592a9ade7b6e8bbb6680851104abfe134af
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-06 16:51:58 +0100

    generic paths for setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup, Extension
 import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+C_HISTOGRAM_FILE = os.path.join(dir_path, 'histogram/histogram_c.c')
+REQUIREMENT_FILE = os.path.join(dir_path, 'requirements.txt')
+README_FILE = os.path.join(dir_path, 'README.md')
 
 histo_module = Extension('histogram.histogram_c',
-                         sources=['histogram/histogram_c.c'])
+                         sources=[C_HISTOGRAM_FILE])
 
 
 if sys.argv[1] == 'install':
@@ -10,12 +16,10 @@ if sys.argv[1] == 'install':
     sys.argv += ['build_ext']
 
 
-REQUIREMENT_FILE = 'requirements.txt'
 with open(REQUIREMENT_FILE) as f:
     content = f.readlines()
 
 requires = [x.strip() for x in content]
-
 setup(
     name='histogram',
     version='0.3.3',
@@ -24,7 +28,7 @@ setup(
     license='GNU GPL 3.0',
     author='Cyril Alispach',
     author_email='cyril.alispach@gmail.com',
-    long_description=open('README.md').read(),
+    long_description=open(README_FILE).read(),
     description='A package for Histogramming',
     requires=requires,
     ext_modules=[histo_module],
